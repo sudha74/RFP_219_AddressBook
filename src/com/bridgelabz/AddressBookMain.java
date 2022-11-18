008366a8aa0d985f589ec2cf2912e746b6195f94
sudha123 <sudhanaukala1997@gmail.com>
2022-11-18 18:16:23 +0530

    Added:CreateContact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System");
+        Contacts contact = new Contacts();
+        contact.setFirstName("Sudha");
+        System.out.println(contact.getFirstName());
     }
 }
