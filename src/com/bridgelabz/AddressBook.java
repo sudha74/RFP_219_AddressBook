d0179b6f362b679c5e99671ae0b33fa93694acac
sudha123 <sudhanaukala1997@gmail.com>
2022-11-18 18:55:20 +0530

    Added:multipleContacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,82 +1,103 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    Contacts contact = new Contacts();
+    ArrayList<Contacts> list = new ArrayList<>();
+
     Scanner scr = new Scanner(System.in);
 
     void addContact() {
-
-        System.out.println("Enter first name");
+        Contacts contact = new Contacts();
+        System.out.print("Enter first name: ");
         contact.setFirstName(scr.next());
-        System.out.println("Enter last name");
+        System.out.print("Enter last name:");
         contact.setLastName(scr.next());
-        System.out.println("Enter city");
+        System.out.print("Enter city: ");
         contact.setCity(scr.next());
-        System.out.println("Enter state");
+        System.out.print("Enter state: ");
         contact.setState(scr.next());
-        System.out.println("Enter address:  ");
+        System.out.print("Enter address:  ");
         scr.nextLine();
-        contact.setAddress(scr.next());
-        System.out.println("Enter Zipcode");
+        contact.setAddress(scr.nextLine());
+        System.out.print("Enter Zipcode: ");
         contact.setZip(scr.nextInt());
-        System.out.println("Enter phone Number");
+        System.out.print("Enter phone Number: ");
         contact.setPhoneNumber(scr.nextLong());
-        System.out.println("Enter email address");
+        System.out.print("Enter email address: ");
         contact.setEmail(scr.next());
-
+        list.add(contact);
     }
 
     void editContact() {
         System.out.println("Enter the first name of person to edit");
-        String firstName = scr.next();
-        if (firstName.equals(contact.getFirstName())) {
-            System.out.println("Edit the details of person");
-            System.out.println("Enter first name");
-            contact.setFirstName(scr.next());
+        String firstName = scr.next().toLowerCase();
 
-            System.out.println("Enter last Name");
-            contact.setLastName(scr.next());
+        boolean found = false;
+        for (Contacts contact : list) {
+            if (firstName.equals(contact.getFirstName().toLowerCase())) {
+                System.out.println("Edit the details of person");
+                System.out.print("Enter first name: ");
+                contact.setFirstName(scr.next());
 
-            System.out.println("Enter Email");
-            contact.setEmail(scr.next());
+                System.out.print("Enter last Name: ");
+                contact.setLastName(scr.next());
 
-            System.out.print("Enter address:  ");
-            scr.nextLine();
-            contact.setAddress(scr.nextLine());
+                System.out.print("Enter Email: ");
+                contact.setEmail(scr.next());
 
-            System.out.println("Enter phone number");
-            contact.setPhoneNumber(scr.nextLong());
+                System.out.print("Enter address: ");
+                scr.nextLine();
+                contact.setAddress(scr.nextLine());
 
-            System.out.println("Enter state");
-            contact.setState(scr.next());
+                System.out.print("Enter phone number: ");
+                contact.setPhoneNumber(scr.nextLong());
 
-            System.out.println("Enter city");
-            contact.setCity(scr.next());
+                System.out.print("Enter state: ");
+                contact.setState(scr.next());
 
-            System.out.println("Enter zip");
-            contact.setZip(scr.nextInt());
+                System.out.print("Enter city: ");
+                contact.setCity(scr.next());
+
+                System.out.print("Enter zip: ");
+                contact.setZip(scr.nextInt());
+                found = true;
+                break;
+            }
         }
-        else {
-            System.out.println("Person not found");
+        if (!found) {
+            System.out.println("No contact found");
         }
     }
+
 
     void deleteContact() {
         System.out.println("Enter the first name of person to delete");
-        String firstName = scr.next();
+        String firstName = scr.next().toLowerCase();
+        boolean found = false;
+        for (Contacts contact : list) {
+            if (firstName.equals(contact.getFirstName().toLowerCase())) {
+                list.remove(contact);
+                found = true;
+                System.out.println("Contact deleted successfully");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No contact found");
+        }
 
-        if (firstName.equals(contact.getFirstName())) {
-            contact = null;
+    }
+
+    void displayContacts() {
+        if (list.isEmpty()) {
+            System.out.println("No contacts to display");
         } else {
-            System.out.println("no contact found");
+            for (Contacts contact : list) {
+                System.out.println(contact);
+            }
         }
     }
-
-    Contacts display() {
-        return contact;
-    }
 }
-
 
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,15 +1,37 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System");
         AddressBook add = new AddressBook();
-
-        add.addContact();
-        System.out.println(add.display());
-        add.editContact();
-        System.out.println(add.display());
-        add.deleteContact();
-        System.out.println(add.display());
+        Scanner scr = new Scanner(System.in);
+        boolean exit = true;
+        do {
+            System.out.println("Enter options:\n1) To add contact\n2) To edit Contact\n3) To display Contacts\n4) To delete contact\n5) To exit");
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    add.addContact();
+                    break;
+                case 2:
+                    add.editContact();
+                    break;
+                case 3:
+                    add.displayContacts();
+                    break;
+                case 4:
+                    add.deleteContact();
+                    break;
+                case 5:
+                    exit = false;
+                default:
+                    break;
+            }
+        } while (exit);
     }
 }
+
+
+
