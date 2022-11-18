816b2c10e40c4e946206006d23f0855c1d1fe558
sudha123 <sudhanaukala1997@gmail.com>
2022-11-18 18:47:49 +0530

    Added:deleteContact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -9,5 +9,7 @@ public class AddressBookMain {
         System.out.println(add.display());
         add.editContact();
         System.out.println(add.display());
+        add.deleteContact();
+        System.out.println(add.display());
     }
 }
